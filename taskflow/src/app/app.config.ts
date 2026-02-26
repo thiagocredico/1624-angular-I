8fe160699330367997e9ca517dc1515c0ad6311d
Ivirson Daltro <ivi.daltro@gmail.com>
2026-02-26 20:49:35 -0300

    Aula 07 - Validações em formulários

--- taskflow/src/app/app.config.ts
+++ taskflow/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
+    provideEnvironmentNgxMask(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
   ],
 };
--- taskflow/src/app/main-panel/pages/transactions/components/create-transaction/create-transaction.component.ts
+++ taskflow/src/app/main-panel/pages/transactions/components/create-transaction/create-transaction.component.ts
@@ -1,15 +1,39 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMaskDirective } from 'ngx-mask';
 import { first } from 'rxjs';
-import { TransactionTypes } from '../../constants/transaction-types.enum';
-import { TransactionsService } from '../../services/transactions.service';
-import { Transaction } from '../../models/transaction.model';
-import { TransactionPagesEnum } from '../../constants/transaction-pages.enum';
 import { RouterService } from '../../../../../core/services/router.service';
+import { TransactionPagesEnum } from '../../constants/transaction-pages.enum';
+import { TransactionTypes } from '../../constants/transaction-types.enum';
+import { Transaction } from '../../models/transaction.model';
+import { TransactionsService } from '../../services/transactions.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-create-transaction',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    NgxMaskDirective,
+    DatePipe,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './create-transaction.component.html',
   styleUrl: './create-transaction.component.css',
 })
@@ -31,14 +55,25 @@ export class CreateTransactionComponent implements OnInit {
     if (this.id) {
       this.getTransactionById();
     }
+
+    this.form
+      .get('date')
+      ?.valueChanges.subscribe(() => console.log(this.form.get('date')));
   }
 
   buildForm(): void {
     this.form = new FormGroup({
-      date: new FormControl(this.todayISO),
-      description: new FormControl(),
-      amount: new FormControl(),
-      type: new FormControl(),
+      date: new FormControl(this.todayISO, [
+        Validators.required,
+        this.dateRangeValidator(new Date(2026, 0, 1), new Date()),
+      ]),
+      description: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100),
+      ]),
+      amount: new FormControl(null, Validators.required),
+      type: new FormControl(null, Validators.required),
     });
   }
 
@@ -101,5 +136,29 @@ export class CreateTransactionComponent implements OnInit {
 
   backToList(): void {
     this.routerService.setTransactionPage(TransactionPagesEnum.LIST);
+  }
+
+  dateRangeValidator(minDate: Date, maxDate: Date): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) return null;
+
+      const value = new Date(control.value);
+
+      if (isNaN(value.getTime())) {
+        return { invalidDate: true };
+      }
+
+      if (value <= minDate || value >= maxDate) {
+        return {
+          dateOutOfRange: {
+            min: minDate,
+            max: maxDate,
+            actual: value,
+          },
+        };
+      }
+
+      return null;
+    };
   }
 }
